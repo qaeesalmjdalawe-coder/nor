e919ac07da3c73a7f9bbc9232a7fe314318ef68e
qaeesalmjdalawe-coder <qaeesalmjdalawe@gmail.com>
2026-09-13 17:51:17 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -9,8 +9,8 @@ const URLS_TO_CACHE = [
     './style.css',
     './app.js',
     './manifest.json',
-    './icons/icon-192.png',
-    './icons/icon-512.png'
+    './icon-192.png',
+    './icon-512.png'
 ];
 
 // ===== التثبيت =====
